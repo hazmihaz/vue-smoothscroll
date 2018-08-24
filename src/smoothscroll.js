baaa11e2637d1f73c55007b97585458fc45c3741
Hazmi Hafiz <hazmihafiz@gmail.com>
2018-08-24 15:34:22 +0700

    add context top offset to startY

--- src/smoothscroll.js
+++ src/smoothscroll.js
@@ -60,7 +60,7 @@
         context = context || window;
         axis = axis === 'both' ? 0b11 : axis === 'y' ? 0b01 : axis === 'x' ? 0b10 : 0b01;
         var enableY = axis & 0b01, enableX = axis & 0b10;
-        var startY = context.scrollTop || window.pageYOffset;
+        var startY = context.scrollTop + context.getBoundingClientRect().top || window.pageYOffset;
         var startX = context.screenLeft || window.pageXOffset;
         if (typeof el === 'number') {
             var endY = enableY && parseInt(el);
